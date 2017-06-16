bdb16c366bc640e036713e09e10d4d20b8e2485d
XieYajie <xyjxyf@163.com>
2017-06-16 11:38:15 +0800

    GameController  and LayerController

---
+++ games/HappyMatch/assets/GameController.js
@@ -0,0 +1,38 @@
+/**
+ * Created by xieyajie on 16/06/2017.
+ */
+
+/*
+* 创建地图
+* */
+let GameController = cc.Class({
+    name: 'GameController',
+
+    properties: {
+        map: Map
+    },
+
+    onLoad: function () {
+        this.initGame();
+    },
+
+    /*
+    * 初始化游戏
+    * */
+    initGame: function () {
+        this.map = require("Map");
+
+        let size = 4;
+        this.map.initMap(size);
+        this.setupCards(size);
+    },
+
+    finishInit: function () {
+        this.map.finishInitMap();
+    },
+
+    setupCards: function (size) {
+
+    },
+
+});
---
+++ games/HappyMatch/assets/LayerController.js
@@ -0,0 +1,21 @@
+/**
+ * Created by xieyajie on 16/06/2017.
+ */
+
+import {Map} from "Map";
+
+/*
+ * 展示地图
+ * */
+let LayerController = cc.Class({
+    name: 'LayerController',
+
+    properties: {
+        
+    },
+
+    startRenderMap: function (map) {
+        
+    },
+
+});
